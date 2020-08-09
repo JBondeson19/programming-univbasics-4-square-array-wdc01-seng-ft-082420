11c2580cd80a4bcb468cc107e7dc570c82992e7e
James P Bondeson <jpbond58@gmail.com>
2020-08-09 18:02:43 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
+require "pry"
+
+
 def square_array(array)
-  # your code here
+	new_numbers = []
+  counter = 0
+while counter < array.size
+  current_item = array[counter]
+new_numbers << current_item ** 2
+
+counter += 1
+
+end
+new_numbers
 end
---
+++ lib/test.rb
@@ -0,0 +1,8 @@
+
+counter = 0
+pets = [1,2,3,4,5]
+
+while pets[counter]  do
+  puts pets[counter]
+  counter += 1
+end
