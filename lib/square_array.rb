3d02cdb36f34c788db8132293157ace4c53be0d7
James P Bondeson <jpbond58@gmail.com>
2020-08-09 17:45:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,12 @@
 require "pry"
 
-binding.pry
+
 def square_array(array)
 	results = []
   counter = 0
 while counter < array.size
   current_item = array[counter]
-
+binding.pry
 current_item ** 2
 
 
