867eb89414925744ed2cbd11becacdf4bb863f8f
James P Bondeson <jpbond58@gmail.com>
2020-08-09 18:01:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,11 @@ require "pry"
 
 
 def square_array(array)
-	results = []
+	new_numbers = []
   counter = 0
 while counter < array.size
   current_item = array[counter]
-results << current_item ** 2
+new_numbers << current_item ** 2
 
 counter += 1
 
