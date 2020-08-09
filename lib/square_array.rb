40309200484521114f2f35319230e63959f8f90f
James P Bondeson <jpbond58@gmail.com>
2020-08-09 17:14:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+require "pry"
+
+
 def square_array(array)
-  # your code here
+
+  counter = [1,4,9]
+  array = [1,2,3]
+
+  while array[counter]  do
+    puts array[counter]
+    counter ** 2
+  end
 end
